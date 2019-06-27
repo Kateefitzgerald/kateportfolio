e21034d35d92db2417c3292c5df5f2bb6a374dbf
Kate Earls FitzGerald <kate.earls.fitzgerald@gmail.com>
2019-06-27 11:58:57 +0100

    cleaning up application js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-require turbolinks
-require jquery
-require jquery_ujs
-require turbolinks
-require bootstrap
-require_tree .
-= require_tree .
+//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
